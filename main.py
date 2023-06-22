af0343a6bda621770a5c53a53c044f225689913a
Artsiom Vaschilo <v2322048@gmail.com>
2023-06-22 10:44:54 +0300

    added waiting for element to appear instead time.sleep

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import time
 
+from config import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import pickle
 import time
@@ -10,7 +13,8 @@ import time
 #  Options
 options = webdriver.ChromeOptions()
 options.add_argument(
-    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 "
+    "Safari/537.36")
 driver = webdriver.Chrome(
     options=options
 )
@@ -22,35 +26,74 @@ def main():
         print(f"[INFO] Download URL", end="")
         driver.get("https://visa.vfsglobal.com/blr/ru/pol/application-detail")
         print(f" >>> OK")
-        time.sleep(10)
-
         # Enter Login and Pass
-        time.sleep(1)
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'))
+        )
+        element.click()
         input_user = f'/html/body/app-root/div/app-login/section/div/div/mat-card/form/div[1]/mat-form-field/div/div[' \
-                    f'1]/div[3]/input '
-        driver.find_element(By.XPATH, value=input_user).send_keys("v2322048@gmail.com")
+                     f'1]/div[3]/input '
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, input_user))
+        )
+        element.send_keys(user)
         input_pass = f'/html/body/app-root/div/app-login/section/div/div/mat-card/form/div[2]/mat-form-field/div/div[' \
                      f'1]/div[3]/input '
-        driver.find_element(By.XPATH, value=input_pass).send_keys("JFRfm5neYAeNQj*")
-        time.sleep(60)
-        print("123")
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, input_pass))
+        )
+        element.send_keys(password)
 
-        # Нажимаем на комбобокс
-        combo = driver.find_element(By.XPATH, '//*[@id="mat-select-0"]/div/div[2]/div')
+        print("[INFO]")
+        # Choose a value ComboBox
+        # Start Button
+        # element = WebDriverWait(driver, 60).until(
+        #     EC.presence_of_element_located((By.XPATH, '/html/body/app-root/div/app-dashboard/section[1]/div/div['
+        #                                               '2]/button/span[1]'))
+        # )
+        # print("[INFO]")
+        # element.click()
+
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mat-select-0"]/div/div[2]/div'))
+        )
+        element.click()
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mat-option-1"]/span')))
+        combo = driver.find_element(By.XPATH, '//*[@id="mat-option-1"]/span')
         combo.click()
-
-        # Rec Cookies
-        time.sleep(100)
-        print(f"[INFO] Save Cookie", end="")
-        pickle.dump(driver.get_cookies(), open("cookies_visa", "wb"))
-        print(" >>> OK")
-        # Download Cookies
-        # print(f"[INFO] Download Cookie", end="")
-        # for cookie in pickle.load(open("cookies", "rb")):
-        #     driver.add_cookie(cookie)
-        # print(" >>> OK")
-        # time.sleep(10)
-        # driver.refresh()
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/app-root/div/app-eligibility-criteria/section/form'
+                                                      '/mat-card[1]/form/div[2]/mat-form-field/div/div[1]')))
+        combo = driver.find_element(By.XPATH, '/html/body/app-root/div/app-eligibility-criteria/section/form/mat'
+                                              '-card[1]/form/div[2]/mat-form-field/div/div[1]')
+        combo.click()
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mat-option-246"]/span')))
+        combo = driver.find_element(By.XPATH, '//*[@id="mat-option-246"]/span')
+        combo.click()
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mat-select-4"]/div/div[2]/div')))
+        combo = driver.find_element(By.XPATH, '//*[@id="mat-select-4"]/div/div[2]/div')
+        combo.click()
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mat-option-252"]/span')))
+        combo = driver.find_element(By.XPATH, '//*[@id="mat-option-252"]/span')
+        combo.click()
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/app-root/div/app-eligibility-criteria/section/form'
+                                                      '/mat-card[1]/form/div[4]/div[3]/input')))
+        combo = driver.find_element(By.XPATH, '/html/body/app-root/div/app-eligibility-criteria/section/form/mat'
+                                              '-card[1]/form/div[4]/div[3]/input')
+        combo.send_keys(DofB)
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mat-select-6"]/div/div[2]')))
+        combo = driver.find_element(By.XPATH, '//*[@id="mat-select-6"]/div/div[2]')
+        combo.click()
+        element = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mat-option-24"]/span')))
+        combo = driver.find_element(By.XPATH, '//*[@id="mat-option-24"]/span')
+        combo.click()
         time.sleep(100)
 
     except Exception as ex:
